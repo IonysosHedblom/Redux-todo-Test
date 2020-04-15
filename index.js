0b7bbbf4161b03d9c9e719ba7736943b278609b0
Ionhedblom <ionysoshedblom@gmail.com>
2020-04-15 20:17:34 +0200

    Changed strings to variables

--- index.js
+++ index.js
@@ -39,15 +39,21 @@ function createStore(reducer) {
 }
 
 // App Code -- This is code that the user might write to decide how the state should change based off of the action
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 // This todos function, is also a REDUCER function. It takes a state and an action, and reducing that to a brand new state
 // TODOS REDUCER
 function todos(state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case ADD_TODO:
       return state.concat([action.todo]);
-    case 'REMOVE_TODO':
+    case REMOVE_TODO:
       return state.filter(todo => todo.id !== action.id);
-    case 'TOGGLE_TODO':
+    case TOGGLE_TODO:
       return state.map(todo =>
         todo.id !== action.id
           ? todo
@@ -62,9 +68,9 @@ function todos(state = [], action) {
 // GOALS REDUCER
 function goals(state = [], action) {
   switch (action.type) {
-    case 'ADD_GOAL':
+    case ADD_GOAL:
       return state.concat([action.goal]);
-    case 'REMOVE_GOAL':
+    case REMOVE_GOAL:
       return state.filter(goal => goal.id !== action.id);
     default:
       return state;
@@ -73,6 +79,7 @@ function goals(state = [], action) {
 
 // Combine reducers to pass to createStore function below.
 // allreducers function invokes todos reducer and goals reducer, they return specific portions of state
+// This is called a root REDUCER
 function allReducers(state = {}, action) {
   return {
     todos: todos(state.todos, action),
@@ -86,10 +93,59 @@ store.subscribe(() => {
 });
 
 store.dispatch({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   todo: {
     id: 0,
-    name: 'Learn  Redux',
+    name: 'Walk the dog',
     complete: false,
   },
+});
+
+store.dispatch({
+  type: ADD_TODO,
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  },
+});
+
+store.dispatch({
+  type: ADD_TODO,
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  },
+});
+
+store.dispatch({
+  type: REMOVE_TODO,
+  id: 1,
+});
+
+store.dispatch({
+  type: TOGGLE_TODO,
+  id: 0,
+});
+
+store.dispatch({
+  type: ADD_GOAL,
+  goal: {
+    id: 0,
+    name: 'Learn Redux',
+  },
+});
+
+store.dispatch({
+  type: ADD_GOAL,
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds',
+  },
+});
+
+store.dispatch({
+  type: REMOVE_GOAL,
+  id: 0,
 });
