f273ef625c4579e113f8873722dd57aad1bb9be4
Ionhedblom <ionysoshedblom@gmail.com>
2020-04-15 17:13:22 +0200

    Dispatch addded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Pure function, based on the specific action that occurs return a new copy of the internal state.
-//  Sets the default value of state to an empty array, otherwise state.concat wont work.
+// Sets the default value of state to an empty array, otherwise state.concat wont work.
 // This todos function, is also a REDUCER function. It takes a state and an action, and reducing that to a brand new state
 function todos(state = [], action) {
   if (action.type === 'ADD_TODO') {
@@ -13,7 +13,7 @@ function createStore() {
   // The store should have four parts
   // 1. The state
   // 2. Get the state
-  // 3. Listen tio changes on the state
+  // 3. Listen to changes on the state
   // 4. Update the state
 
   // The state (nr 1)
@@ -23,6 +23,7 @@ function createStore() {
   // Get the state (nr 2)
   const getState = () => state;
 
+  // A way to listen to changes on the state (nr 3)
   const subscribe = listener => {
     listeners.push(listener);
     return () => {
@@ -30,10 +31,17 @@ function createStore() {
     };
   };
 
+  // Dispatch is responsible for updating the state inside store. The action argument is going to tell dispatch which specific event occured inside app.
+  const dispatch = action => {
+    state = todos(state, action);
+    listeners.forEach(listener => listener());
+  };
+
   // Whenever createStore is invoked, return an object
   return {
     getState,
     subscribe,
+    dispatch,
   };
 }
 
